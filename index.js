d3cfa33c9c7b35aa6a3249284ae7e167bcde43f6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-10-21 09:14:46 -0400

    exporting for browser

--- index.js
+++ index.js
@@ -1,59 +1,54 @@
-// var expect = require('expect.js');
+;(function initHeroin() {
+  'use strict';
 
-function getExpectedArguments(fn) {
-  // expect(fn).to.be.a('function');
-
-  var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
-  var text = fn.toString();
-  // expect(text).to.be.a('string');
-  var args = text.match(FN_ARGS)[1].split(',');
-  args = args.map(function (name) {
-    return name.trim();
-  });
-  return args;
-}
-
-function heroin(obj, methodName, dependencies) {
-  var original;
-
-  if (typeof obj === 'function') {
-    original = obj;
-    dependencies = methodName;
-  } else {
-    // expect(obj).to.be.an('object');
-    // expect(methodName).to.be.a('string');
-    original = obj[methodName];
-  }
-
-  /*
-  if (dependencies) {
-    expect(dependencies).to.be.an('object');
-  }*/
-  dependencies = dependencies || {};
-
-  // expect(original).to.be.a('function');
-
-  var expected = getExpectedArguments(original);
-  // expect(expected).to.be.an('array');
-  // console.log('expected', expected);
-
-  var proxy = function () {
-    var runtimeArgs = Array.prototype.slice.call(arguments, 0);
-    var parameters = expected.map(function (name) {
-      if (dependencies.hasOwnProperty(name)) {
-        return dependencies[name];
-      } else {
-        return runtimeArgs.shift();
-      }
+  function getExpectedArguments(fn) {
+    var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
+    var text = fn.toString();
+    var args = text.match(FN_ARGS)[1].split(',');
+    args = args.map(function (name) {
+      return name.trim();
     });
-    // console.log('parameters', parameters);
-    return original.apply(obj, parameters);
-  };
-
-  if (typeof obj !== 'function') {
-    obj[methodName] = proxy;
+    return args;
   }
-  return proxy;
-}
 
-module.exports = heroin;
+  function heroin(obj, methodName, dependencies) {
+    var original;
+
+    if (typeof obj === 'function') {
+      original = obj;
+      dependencies = methodName;
+    } else {
+      original = obj[methodName];
+    }
+
+    dependencies = dependencies || {};
+
+    var expected = getExpectedArguments(original);
+
+    var proxy = function () {
+      var runtimeArgs = Array.prototype.slice.call(arguments, 0);
+      var parameters = expected.map(function (name) {
+        if (dependencies.hasOwnProperty(name)) {
+          return dependencies[name];
+        } else {
+          return runtimeArgs.shift();
+        }
+      });
+      // console.log('parameters', parameters);
+      return original.apply(obj, parameters);
+    };
+
+    if (typeof obj !== 'function') {
+      obj[methodName] = proxy;
+    }
+    return proxy;
+  }
+
+  if (typeof module === 'object') {
+    module.exports = heroin;
+  }
+  if (typeof window === 'object') {
+    /* global window: true */
+    window.heroin = heroin;
+  }
+}());
